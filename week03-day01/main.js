41cb660e150f33983c7d39f6946b8d164f4bb92c
Matt Calthrop <mcalthrop@gmail.com>
2017-02-27 15:44:13 +0000

    add JS control flow

--- week03-day01/main.js
+++ week03-day01/main.js
@@ -4,14 +4,14 @@ var s = '';
 var b = true;
 var n = 1;
 var a = [];
-console.log('Using "typeof" operator:');
+console.log('--- Using "typeof" operator:');
 console.log(typeof o);
 console.log(typeof s);
 console.log(typeof b);
 console.log(typeof n);
 console.log(typeof a);
 
-console.log('Playing with different types:');
+console.log('--- Playing with different types:');
 var numberAsString = '5';
 var number1 = 3;
 var number2 = 7;
@@ -31,3 +31,44 @@ console.log(parseFloat(3.1415927));
 console.log(parseFloat('3.1415927'));
 console.log(parseFloat(3.56));
 console.log(parseFloat('blah'));
+
+console.log('--- Control flow:');
+var moneyInPocket = 20;
+
+if (moneyInPocket > 10) {
+  console.log('Another drink please!');
+} else if (moneyInPocket > 5) {
+  console.log('Make mine a half then');
+} else {
+  console.log('Time to go home');
+}
+
+var countryOfOrigin = 'Kenya';
+var greeting = '';
+
+switch(countryOfOrigin) {
+  case 'France':
+    greeting = 'Bonjour';
+    break;
+  case 'Spain':
+    greeting = '¡Hola!';
+    break;
+  case 'Indonesia':
+    greeting = 'Selamat sore';
+    break;
+  case 'Kenya':
+    greeting = 'Jambo!';
+    break;
+  default:
+    greeting = 'Well hello there';
+    break;
+}
+console.log(greeting);
+
+var dayOfWeek = 'Thursday';
+// logical AND
+if (dayOfWeek.toUpperCase() === 'THURSDAY' || dayOfWeek.toUpperCase() === 'FRIDAY') {
+  console.log('Pub thisarvo!');
+} else {
+  console.log('Bummer :-(');
+}
