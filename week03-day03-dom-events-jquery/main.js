fe1d9c55f78357d283527774feb1b98fcddb0873
Matt Calthrop <mcalthrop@gmail.com>
2017-03-01 16:05:55 +0000

    add DOM event stuff

--- week03-day03-dom-events-jquery/main.js
+++ week03-day03-dom-events-jquery/main.js
@@ -1,30 +1,42 @@
 console.log('in main.js');
 
-console.log(document);
+document.addEventListener('DOMContentLoaded', function () {
+  console.log('DOM loaded');
+  console.log(document);
 
-console.log('--- using querySelectorAll:');
-var selectedListItems = document.querySelectorAll('.selected');
+  console.log('--- using querySelectorAll:');
+  var selectedListItems = document.querySelectorAll('.selected');
 
-console.log('selectedListItems:', selectedListItems);
+  console.log('selectedListItems:', selectedListItems);
 
-for(var i = 0 ; i < selectedListItems.length ; i++) {
-  console.log('selected list item style:', selectedListItems[i].style);
-  selectedListItems[i].style.color = 'red';
-}
+  for(var i = 0 ; i < selectedListItems.length ; i++) {
+    console.log('selected list item style:', selectedListItems[i].style);
+    selectedListItems[i].style.color = 'red';
+  }
 
-console.log('--- Create and append a new element');
-var newListItem = document.createElement('li');
+  console.log('--- Create and append a new element');
+  var newListItem = document.createElement('li');
 
-newListItem.innerHTML = 'in New York';
-newListItem.setAttribute('class', 'muted');
+  newListItem.innerHTML = 'in New York';
+  newListItem.setAttribute('class', 'muted');
 
-// We are going to call .appendChild() on the parent <ul> element.
-// That <ul> element has an id attribute.
+  // We are going to call .appendChild() on the parent <ul> element.
+  // That <ul> element has an id attribute.
 
-// So we retrieve the DOM element that contains the list items,
-// because we want to call .appendChild() on it.
-// NOTE: we do NOT pass a CSS selector ('#item-list') to the
-// .getElementById() method.
-var listContainer = document.getElementById('item-list');
+  // So we retrieve the DOM element that contains the list items,
+  // because we want to call .appendChild() on it.
+  // NOTE: we do NOT pass a CSS selector ('#item-list') to the
+  // .getElementById() method.
+  var listContainer = document.getElementById('item-list');
 
-listContainer.appendChild(newListItem);
+  listContainer.appendChild(newListItem);
+
+  console.log('--- DOM events');
+  var pickMeButton = document.getElementById('pickMeBtn');
+
+  pickMeButton.addEventListener('click', function () {
+    pickMeButton.innerHTML = 'CLICKED';
+    alert('Picked!');
+  });
+
+});
