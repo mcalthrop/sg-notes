3e9f49448e5c79865976aff8f78e047b3310e2ae
Matt Calthrop <mcalthrop@gmail.com>
2017-03-02 14:12:06 +0000

    add jQuery

--- week03-day03-dom-events-jquery/main.js
+++ week03-day03-dom-events-jquery/main.js
@@ -1,7 +1,7 @@
 console.log('in main.js');
 
 document.addEventListener('DOMContentLoaded', function () {
-  console.log('DOM loaded');
+  console.log('DOM loaded (with DOMContentLoaded event)');
   console.log(document);
 
   console.log('--- using querySelectorAll:');
@@ -39,4 +39,48 @@ document.addEventListener('DOMContentLoaded', function () {
     alert('Picked!');
   });
 
+});
+
+$(function () {
+  console.log('DOM loaded (jQuery)');
+  console.log('--- using jQuery');
+
+  var listContainer1 = document.getElementById('item-list');
+  // It's useful to have a convention of naming the result of a jQuery call
+  // with a '$' prefix (as below).
+  // That way, it is immediately obvious that the variable is a jQuery object,
+  // and therefore has all the jQuery goodies on it.
+  // (But it's only a convention, not a rule).
+  var $listContainer2 = $('#item-list');
+  var $listItems = $('#item-list li');
+
+  console.log('listContainer1 (document.getElementById) :', listContainer1);
+  console.log('listContainer2 (jQuery):', $listContainer2);
+  console.log('listItems (jQuery):', $listItems);
+
+  var newListItem1 = $('<li>new item appended by jQuery</li>').addClass('garish');
+  var newListItem2 = $('<li></li>').addClass('dull').html('new item prepended by jQuery');
+  console.log('newListItem2.html():', newListItem2.html());
+  $listContainer2.append(newListItem1);
+  $listContainer2.prepend(newListItem2);
+
+  // Now let's add the 'emphasised' CSS class to all the <li> elements.
+
+  // Without jQuery, you'd have to do something like this:
+  // var listItems2 = document.getElementsByTagName('li');
+  // for (var i = 0 ; i < listItems2.length ; i++) {
+  //   listItems2[i].classList.add('emphasised');
+  // }
+
+  // And with jQuery, it's a lot more simple:
+  $listContainer2.children().addClass('emphasised');
+
+  // now to add a click handler to the button
+  $('#pickMeBtn').on('click', function() {
+    console.log('jQuery .on("click") handler');
+  });
+  // and this works too:
+  $('#pickMeBtn').click(function() {
+    console.log('jQuery .click() handler');
+  });
 });
