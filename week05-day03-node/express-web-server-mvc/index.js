7aec3a825654680119fe9097deb7210836cc493a
Matt Calthrop <mcalthrop@gmail.com>
2017-03-15 12:48:33 +0000

    implement put and delete with method-override

--- week05-day03-node/express-web-server-mvc/index.js
+++ week05-day03-node/express-web-server-mvc/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = require('./config/router');
 var bodyParser = require('body-parser');
 var layouts = require('express-ejs-layouts');
+var methodOverride = require('method-override');
 var app = express();
 var port = 3000;
 
@@ -14,6 +15,14 @@ app.use(function (req, res, next) {
 app.use(layouts);
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(methodOverride(function (req) {
+  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+    // look in urlencoded POST bodies and delete it
+    var method = req.body._method;
+    delete req.body._method;
+    return method;
+  }
+}));
 
 app.use(router);
 
