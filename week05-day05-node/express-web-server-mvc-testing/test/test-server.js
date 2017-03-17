b33c7e9bfbacccf8eb44b4a8a28935fb6c9f0878
Matt Calthrop <mcalthrop@gmail.com>
2017-03-17 16:22:22 +0000

    add test for invalid URL

--- week05-day05-node/express-web-server-mvc-testing/test/test-server.js
+++ week05-day05-node/express-web-server-mvc-testing/test/test-server.js
@@ -1,18 +1,32 @@
-/* global describe, it */
+/* global describe, it, beforeEach */
 
 var chai = require('chai');
 var chaiHttp = require('chai-http');
 var app = require('../index');
+var expect = chai.expect;
+var request;
 
+chai.should();
 chai.use(chaiHttp);
 
 describe('Users', function () {
-  it('should list all users for GET /users', function (done) {
-    var request = chai.request(app);
+  beforeEach(function () {
+    request = chai.request(app);
+  });
 
+  it('should return error for invalid URL GET', function (done) {
+    request
+      .get('/invalid_url')
+      .end(function (err) {
+        expect(err).not.to.be.null;
+        done();
+      });
+  });
+  it('should list all users for GET /users', function (done) {
     request
       .get('/users')
       .end(function (err, res) {
+        expect(err).to.be.null;
         res.should.have.status(200);
         res.should.be.html;
         done();
