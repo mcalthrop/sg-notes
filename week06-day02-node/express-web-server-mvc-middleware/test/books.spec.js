a9f69670d9e52f249b99a1e3f9c390de58005556
Matt Calthrop <mcalthrop@gmail.com>
2017-03-22 17:28:40 +0000

    add tests for PUT books

--- week06-day02-node/express-web-server-mvc-middleware/test/books.spec.js
+++ week06-day02-node/express-web-server-mvc-middleware/test/books.spec.js
@@ -14,6 +14,46 @@ describe('Books', function () {
     request = chai.request(app);
   });
 
+  describe('PUT', function () {
+    it('should return error for non-existent book', function (done) {
+      request
+        .put('/books/non-existent-book-id')
+        .end(function (err, res) {
+          res.should.have.status(404);
+          done();
+        });
+    });
+    it('should return correct result for existing book', function (done) {
+      request
+        .get('/users')
+        .end(function (err, res) {
+          var userId = TestUtils.getFirstUserIdFromUserListHTML(res.text);
+
+          request
+            .get('/users/' + userId)
+            .end(function (err, res) {
+              var bookId = TestUtils.getFirstBookIdFromUserPageHTML(res.text);
+              var testTitle = TestUtils.generateUniqueString('title');
+              var testAuthor = TestUtils.generateUniqueString('author');
+
+              request
+                .put('/books/' + bookId)
+                .set('Content-Type', 'application/x-www-form-urlencoded')
+                .send({ title: testTitle, author: testAuthor, userId: userId})
+                .end(function (err, res) {
+                  var titleRegExp = new RegExp(testTitle);
+                  var authorRegExp = new RegExp(testAuthor);
+
+                  res.should.have.status(200);
+                  res.text.should.match(titleRegExp);
+                  res.text.should.match(authorRegExp);
+                  done();
+                });
+            });
+        });
+    });
+  });
+
   describe('DELETE', function () {
     it('should return error for non-existent book id', function (done) {
       request
