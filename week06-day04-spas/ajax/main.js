94772ab762c977f450e21c35332801a23b1508a5
Matt Calthrop <mcalthrop@gmail.com>
2017-03-23 12:26:34 +0000

    add Chuck Norris quotes randomiser

---
+++ week06-day04-spas/ajax/main.js
@@ -0,0 +1,37 @@
+$(function () {
+  console.log('page is loaded');
+
+  function ajaxTheManualWay() {
+    var request = new XMLHttpRequest();
+
+    console.log('AJAX the manual way');
+    request.open('GET', 'http://api.icndb.com/jokes/random');
+    request.addEventListener('load', function () {
+      var json = JSON.parse(this.responseText);
+      var jokeElement = document.getElementById('joke');
+
+      jokeElement.innerHTML = json.value.joke;
+    });
+    request.send();
+  }
+
+  function ajaxTheJQueryWay() {
+    console.log('AJAX the jQuery way');
+
+    $.get('http://api.icndb.com/jokes/random', function (data) {
+      $('#joke').html(data.value.joke);
+    });
+  }
+
+  // source: http://stackoverflow.com/a/5915122/155206
+  function selectRandomElement (items) {
+    return items[Math.floor(Math.random() * items.length)];
+  }
+
+  var ajaxFunctions = [ajaxTheManualWay, ajaxTheJQueryWay];
+  setInterval(function () {
+    var randomAjaxFunction = selectRandomElement(ajaxFunctions);
+
+    randomAjaxFunction();
+  }, 5000);
+});
