206b6ceaa8a41908d80fd0244a0e252d677548ec
Matt Calthrop <mcalthrop@gmail.com>
2017-03-29 17:11:31 +0100

    add angular controller and other directives

--- week07-day03-angular/angular-intro/app.js
+++ week07-day03-angular/angular-intro/app.js
@@ -1,4 +1,4 @@
 console.log('app.js: angular:', angular);
 
-var app = angular.module('myFirstApp', []);
+angular.module('myFirstApp', []);
 
---
+++ week07-day03-angular/angular-intro/controllers/home.controller.js
@@ -0,0 +1,55 @@
+console.log('home.controller.js');
+
+function HomeController() {
+  var controller = this;
+  var canShowGonzo = false;
+
+  // Shows example of sending the click event to this handler –
+  // see the markup, where the variable `$event` is passed to this method.
+  controller.showGonzo = function(event) {
+    console.log('showGonzo: event:', event);
+    canShowGonzo = true;
+  };
+
+  controller.hideGonzo = function() {
+    canShowGonzo = false;
+  };
+
+  controller.toggleGonzo = function () {
+    canShowGonzo = !canShowGonzo;
+  };
+
+  controller.isGonzoVisible = function () {
+    return canShowGonzo;
+  };
+
+  controller.getGonzoVisibilityClass = function () {
+    var className = 'isVisible';
+
+    if (!canShowGonzo) {
+      className = 'isNotVisible';
+    }
+
+    return className;
+  };
+
+  controller.addTrainer = function () {
+    console.log('addTrainer: controller.newTrainerName:', controller.newTrainerName);
+    controller.trainers.push(controller.newTrainerName);
+    controller.newTrainerName = '';
+  };
+
+  function init() {
+    console.log('inside HomeController');
+    controller.newTrainerName = '';
+    controller.title = 'Home page';
+    controller.trainers = ['Steve', 'Matt', 'Ollie', 'Niall'];
+    controller.hideGonzo();
+  }
+
+  init();
+}
+
+angular
+  .module('myFirstApp', [])
+  .controller('HomeController', HomeController);
