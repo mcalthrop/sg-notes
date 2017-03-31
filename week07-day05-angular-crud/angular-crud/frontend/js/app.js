1160745d533a6d0dbcc19be261a9147bf6b7b648
Matt Calthrop <mcalthrop@gmail.com>
2017-03-31 11:09:08 +0100

    formatting changes etc

--- week07-day05-angular-crud/angular-crud/frontend/js/app.js
+++ week07-day05-angular-crud/angular-crud/frontend/js/app.js
@@ -1,14 +1,13 @@
 angular
   .module('DuckApp', ['ui.router'])
-  .config(MainRouter)
+  .config(MainRouter);
 
 function MainRouter ($stateProvider, $urlRouterProvider) {
   $stateProvider
     .state('home', {
       url: '/ducks',
       templateUrl: '/states/home.html'
-    })
+    });
 
-  $urlRouterProvider.otherwise('/ducks')
-
+  $urlRouterProvider.otherwise('/ducks');
 }
