3ccafc6f9da2aa4475ac2260b508614c7bd25087
Matt Calthrop <mcalthrop@gmail.com>
2017-04-03 16:43:16 +0100

    implement route protection

--- week08-day01-angular-authentication/angular-firebase-authentication/public/js/app.js
+++ week08-day01-angular-authentication/angular-firebase-authentication/public/js/app.js
@@ -14,16 +14,35 @@ function MainRouter ($stateProvider, $urlRouterProvider) {
       url: '/login',
       templateUrl: '/states/login.html'
     })
-    .state('authRequired', {
+    .state('auth-required', {
       url: '/authrequired',
-      templateUrl: '/states/authRequired.html'
+      templateUrl: '/states/auth-required.html'
     })
     .state('secret', {
       url: '/secret',
-      templateUrl: '/states/secret.html'
+      templateUrl: '/states/secret.html',
+      resolve: {
+        currentAuth: [
+          'AuthFactory',
+          function (AuthFactory) {
+            return AuthFactory.$requireSignIn();
+          }
+        ]
+      }
     });
 }
+MainRouter.$inject = ['$stateProvider', '$urlRouterProvider'];
+
+function AuthCatcher($rootScope, $state) {
+  $rootScope.$on('$stateChangeError', (event, toState, toParams, fromState, fromParams, error) => {
+    if (error === 'AUTH_REQUIRED') {
+      $state.go('auth-required');
+    }
+  });
+}
+AuthCatcher.$inject = ['$rootScope', '$state'];
 
 angular
   .module('myApp', ['ui.router', 'firebase'])
-  .config(MainRouter);
+  .config(MainRouter)
+  .run(AuthCatcher);
